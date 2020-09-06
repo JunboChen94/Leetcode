4157c8feab783cbeacf58e7681f79f0efdb2c76b
Junbo Chen <jc4648@columbia.edu>
2020-09-06 11:48:51 -0400

    LC1428:Leftmost-Column-with-at-Leasr-a-One:binary-search

---
+++ junbo/LC_1428.py
@@ -0,0 +1,33 @@
+class Solution:
+def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+    '''
+    Binary Search: discussion, not as good as mine
+    '''
+    n, m = binaryMatrix.dimensions()
+    res = m
+    for j in range(n):
+        lo, hi = 0, m
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if binaryMatrix.get(j, mid) < 1:
+                lo = mid + 1
+            else:
+                hi = mid
+        res = min(res, lo)
+    return res if res < m else -1
+    '''
+    Mine is better, do not need to run full binary for each every row
+    binary search need to find first column where there exist row making [row, column] none zero
+    '''
+    H, W = binaryMatrix.dimensions()
+    candidate_rows = list(range(H))
+    l, r = 0, W
+    while l < r:
+        mid = (l + r) // 2
+        nonzero_rows = [candidate_row for candidate_row in candidate_rows if binaryMatrix.get(candidate_row, mid)]
+        if len(nonzero_rows):
+            candidate_rows = nonzero_rows
+            r = mid
+        else:
+            l = mid + 1
+    return (-1, r)[r < W]
