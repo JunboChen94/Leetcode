46150657784887527d1250c27a4ef794a4d58039
Junbo Chen <jc4648@columbia.edu>
2020-09-17 21:37:02 -0400

    LC_382:Linked List Random Node

--- junbo/LC_382.py
+++ junbo/LC_382.py
@@ -1,16 +1,25 @@
-class Solution:
-'''
-simple reservoir sampling, K=1
-'''
-def __init__(self, nums: List[int]):
-    self.nums = nums
 
-def pick(self, target: int) -> int:
-    ret, count = None, 0
-    for i, num in enumerate(self.nums):
-        if not (num == target):
-            continue
-        count += 1
-        if random.randint(1, count) == 1:
-            ret = i
-    return ret
+class Solution:
+    '''
+    simple reservoir sampling, K=1
+    '''
+    def __init__(self, head: ListNode):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        """
+        self.head = head
+        
+
+    def getRandom(self) -> int:
+        """
+        Returns a random node's value.
+        """
+        ret, count = None, 0
+        p = self.head
+        while p:
+            count += 1
+            if random.randint(1, count) == 1:
+                ret = p.val
+            p = p.next
+        return ret
